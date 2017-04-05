249f2c6c5147636ebd3c9b074be5ec8dda0864cd
Jack Niall Jones <jjwelsh95@gmail.com>
2017-04-05 12:39:19 +0100

    Add hhgttg .txt && .py
    Added a larger body of text to generate sentences
    Crowley wrote too much in french

---
+++ hhgttg.py
@@ -0,0 +1,17 @@
+#! python3
+# markovify - nice little library to make markov chains out of text files
+# source of markovify https://github.com/jsvine/markovify
+import markovify
+
+# Get raw text as string.
+with open("hhgttg.txt") as f:
+    text = f.read()
+
+# Build the model
+text_model = markovify.Text(text)
+
+# Print five randomly generated sentences
+for i in range(5):
+    print(text_model.make_sentence(tries=9001))
+
+
