f78bea1770e1e7544be475b11027d86ceb5420ca
Jack Niall Jones <jjwelsh95@gmail.com>
2017-04-05 12:16:52 +0100

    Add basic code for markov chain

--- whitestains.py
+++ whitestains.py
@@ -1,4 +1,17 @@
 #! python3
-#markovify - nice little library to make markov chains out of text files
+# markovify - nice little library to make markov chains out of text files
 # source of markovify https://github.com/jsvine/markovify
+import markovify
+
+# Get raw text as string.
+with open("dedicace.txt") as f:
+    text = f.read()
+
+# Build the model
+text_model = markovify.Text(text)
+
+# Print five randomly generated sentences
+for i in range(5):
+    print(text_model.make_sentence())
+
 
